b8d0924547348701d9e2ef97f2d0378c66f00ff1
Kissdani05 <kissdani05@gmail.com>
2024-10-11 11:04:14 +0200

    Organizer kezdet

--- CasinoPRO/AdminPanel.xaml.cs
+++ CasinoPRO/AdminPanel.xaml.cs
@@ -47,48 +47,7 @@ namespace CasinoPRO
         public AdminPanel()
         {
             InitializeComponent();
-
-            //Loading users
             LoadUsersFromDatabase();
-
-            // Sportok és fogadások listájának létrehozása
-            Sports = new ObservableCollection<Sport>
-            {
-                new Sport("Foci")
-                {
-                    Bets = new ObservableCollection<Bet>
-                    {
-                        new Bet { Match = "Csapat A vs Csapat B", HomeOdds = 1.50m, AwayOdds = 2.50m, DrawOdds = 3.00m },
-                        new Bet { Match = "Csapat C vs Csapat D", HomeOdds = 1.75m, AwayOdds = 2.00m, DrawOdds = 3.50m }
-                    }
-                },
-                new Sport("Kosárlabda")
-                {
-                    Bets = new ObservableCollection<Bet>
-                    {
-                        new Bet { Match = "Team A vs Team B", HomeOdds = 1.20m, AwayOdds = 3.50m, DrawOdds = 2.10m }
-                    }
-                },
-                new Sport("Tenisz")
-                {
-                    Bets = new ObservableCollection<Bet>
-                    {
-                        new Bet { Match = "Player A vs Player B", HomeOdds = 1.30m, AwayOdds = 3.10m, DrawOdds = 0.00m }
-                    }
-                },
-                new Sport("Hoki")
-                {
-                    Bets = new ObservableCollection<Bet>
-                    {
-                        new Bet { Match = "Team X vs Team Y", HomeOdds = 2.50m, AwayOdds = 1.80m, DrawOdds = 3.20m }
-                    }
-                }
-            };
-
-            // Alapértelmezett sport a Foci
-            SelectedSport = Sports[0];
-
-            // Adatkötés a Gridhez
             DataContext = this;
         }
 
@@ -292,16 +251,11 @@ namespace CasinoPRO
         // Profilok gomb kezelése
         private void Profilok_Click(object sender, RoutedEventArgs e)
         {
-            SportsGrid.Visibility = Visibility.Collapsed;
             ProfileGrid.Visibility = Visibility.Visible;
         }
 
         // Sportok gomb kezelése
-        private void Sportok_Click(object sender, RoutedEventArgs e)
-        {
-            ProfileGrid.Visibility = Visibility.Collapsed;
-            SportsGrid.Visibility = Visibility.Visible;
-        }
+        
 
         // Kijelentkezés
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -309,56 +263,5 @@ namespace CasinoPRO
             this.Close();
         }
 
-        // Új sport hozzáadása
-        private void AddNewSport_Click(object sender, RoutedEventArgs e)
-        {
-            string sportName = Microsoft.VisualBasic.Interaction.InputBox("Add meg az új sport nevét:", "Új sport hozzáadása", "Új sport");
-
-            if (!string.IsNullOrWhiteSpace(sportName))
-            {
-                var newSport = new Sport(sportName);
-                Sports.Add(newSport);
-                SelectedSport = newSport;
-            }
-        }
-
-        // Új fogadás hozzáadása a kiválasztott sporthoz
-        private void AddNewBet_Click(object sender, RoutedEventArgs e)
-        {
-            if (SelectedSport != null)
-            {
-                SelectedSport.Bets.Add(new Bet { Match = "Új valaki vs valaki", HomeOdds = 1.00m, AwayOdds = 2.00m, DrawOdds = 3.00m });
-            }
-        }
-
-        // Sport kiválasztása
-        private void SportSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var comboBox = sender as ComboBox;
-            var selectedSport = comboBox.SelectedItem as Sport;
-
-            if (selectedSport != null)
-            {
-                SelectedSport = selectedSport;
-            }
-        }
-
-        // Fogadás mentése
-        private void SaveBet_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("A fogadás mentve!");
-        }
-
-        // Fogadás törlése
-        private void DeleteBet_Click(object sender, RoutedEventArgs e)
-        {
-            var button = sender as Button;
-            var bet = button.DataContext as Bet;
-
-            if (bet != null && SelectedSport != null)
-            {
-                SelectedSport.Bets.Remove(bet);
-            }
-        }
     }
 }
